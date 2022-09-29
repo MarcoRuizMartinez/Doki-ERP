4a31a5c0783abbe4e2aaf5d9f4e9ba3c2a1d2c8b
MarcoRuizMartinez <42611365+MarcoRuizMartinez@users.noreply.github.com>
2022-09-29 08:48:07 -0500

    22-09-29-A

--- src/areas/sistemas/components/ModuloLinks.vue
+++ src/areas/sistemas/components/ModuloLinks.vue
@@ -1,49 +1,111 @@
 <template>
-    <ventana                      minimizar
-      class-contenido             ="column items-center"
-      titulo                      ="Enlaces internos"
-      icono                       ="mdi-link"
-      size-icon-carga             ="14em"
-      padding-contenido           ="0"
-      :cargando                   ="!enlaces.length"
+    <ventana                minimizar
+      class-contenido       ="column items-center"
+      titulo                ="Enlaces internos"
+      icono                 ="mdi-link"
+      size-icon-carga       ="14em"
+      padding-contenido     ="0"
+      :cargando             ="!enlaces.length"
       >
-      <q-table                    borbordered dense flat
-        class                     ="fit tabla-maco"
-        row-key                   ="id"
-        :rows                     ="enlaces"
-        :columns                  ="columnas"
-        >
-      </q-table>
+      <q-list>
+        <q-item             clickable v-ripple
+          v-for             ="enlace in enlaces"
+          class             ="q-my-sm"
+          :to               ="enlace.array_options.options_url"
+          :key              ="enlace.ref"
+          >
+          <q-item-section   avatar>
+            <q-avatar
+              color         ="primary"
+              text-color    ="white"
+              >
+              {{ enlace.question[0] }}
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              {{ enlace.question }}
+            </q-item-label>
+            <q-item-label caption>
+              {{ enlace.answer }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </ventana>
+
+<!--     <q-separator spaced />
+  overline
+  caption
+
+  question
+  ref
+  answer
+  array_options.options_url
+  options_grupos_usuarios
+  options_url -->
   </template>
-  
+
   <script setup lang="ts">
-    import {  ref, onMounted          } from "vue"
-    import {  IColumna,
-              Columna                 } from "src/models/Tabla"
-    import    ventana                   from "components/utilidades/Ventana.vue"
-    import {  useApiDolibarr        } from "src/services/useApiDolibarr"      
+  import {  ref, onMounted          } from "vue"
+  import    ventana                   from "components/utilidades/Ventana.vue"
+  import {  useApiDolibarr        } from "src/services/useApiDolibarr"      
+  ////////////////////////////////////////////////////////////////////////// Store
+  import {  storeToRefs         } from 'pinia'
+  import {  useStoreUser        } from 'src/stores/user'
 
-    const { apiDolibarr       } = useApiDolibarr()
-    const enlaces               = ref<any[]>([])
+  const { usuario           } = storeToRefs( useStoreUser() )       
+  const { apiDolibarr       } = useApiDolibarr()
+  const enlaces               = ref<any[]>([])
 
-    onMounted(cargarEnlaces)
+  onMounted(cargarEnlaces)
 
-    async function cargarEnlaces(){
-      const { ok, data }        = await apiDolibarr( "buscar", "saber" )
-      if(ok){
-        if(Array.isArray(data))
-          enlaces.value = data
-          console.log("enlaces.value: ", enlaces.value);
-      }
+  async function cargarEnlaces(){
+    const { ok, data }        = await apiDolibarr( "buscar", "saber" )
+    if(ok){
+      if(Array.isArray(data))
+        enlaces.value = data
+        console.log("enlaces.value: ", enlaces.value);
     }
+  }
 
 
 
-    const columnas: IColumna[]  = [
-                                    new Columna({ name: "ref"}),
-                                    new Columna({ name: "question"}),
-                                    new Columna({ name: "sigla"}),
-                                    new Columna({ name: "grupo"}),
-                                  ]
+
+    const contacts = [ {
+  id: 1,
+  name: 'Ruddy Jedrzej',
+  email: 'rjedrzej0@discuz.net',
+  letter: 'R'
+}, {
+  id: 2,
+  name: 'Mallorie Alessandrini',
+  email: 'malessandrini1@marketwatch.com',
+  letter: 'M'
+}, {
+  id: 3,
+  name: 'Elisabetta Wicklen',
+  email: 'ewicklen2@microsoft.com',
+  letter: 'E'
+}, {
+  id: 4,
+  name: 'Seka Fawdrey',
+  email: 'sfawdrey3@wired.com',
+  letter: 'S'
+} ]
+
+const offline = [ {
+  id: 5,
+  name: 'Brunhilde Panswick',
+  email: 'bpanswick4@csmonitor.com',
+  avatar: 'avatar2.jpg'
+}, {
+  id: 6,
+  name: 'Winfield Stapforth',
+  email: 'wstapforth5@pcworld.com',
+  avatar: 'avatar6.jpg'
+} ]
+
+
+
   </script>
