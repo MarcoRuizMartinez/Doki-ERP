d64eb05f1d832cd9687d8ea1c5fcad938f41020f
MarcoRuizMartinez <42611365+MarcoRuizMartinez@users.noreply.github.com>
2022-10-31 06:33:47 -0500

    22-10-31-A

---
+++ src/areas/acuerdos/pages/oc_proveedor/PageOCBuscar.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding class         ="row item-stretch content-start justify-start">
+    <vista-busqueda
+      icono                     ="mdi-cart"
+      :tipo                     ="TIPO_ACUERDO.PEDIDO"
+    />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import {  useTitle            } from "@vueuse/core"
+  import    vistaBusqueda         from "src/areas/acuerdos/components/Views/VistaAcuerdoBusqueda.vue" 
+  import {  TIPO_ACUERDO        } from "src/areas/acuerdos/models/ConstantesAcuerdos"
+
+  useTitle("🔍 Buscar cotizaciones")
+</script>
